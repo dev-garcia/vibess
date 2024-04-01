7f2192cbef1994d0f0925faaa8c0440a44535447
Cristian Garcia <garcia.ac.cristian@gmail.com>
2024-04-01 19:28:31 -0400

    chore: deleta o antigo projeto

--- src/app/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="mt-60 font-sans text-white md:mt-80">
-      <p className="">© 2023. Cristian Garcia.</p>
-    </footer>
-  );
-};
-
-export default Footer;
