af6d0d21c90abc6d744f46498fcc0b6cab1956ea
Cristian Garcia <garcia.ac.cristian@gmail.com>
2024-04-01 19:40:42 -0400

    chore: instala o next js na versão 14.1.4 e altera as meta tags.

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Vibess",
+  description:
+    "Vibess é um Web Player de música sem fins lucrativos, desenvolvido por Cristian Garcia, com intuito de aprender novas habilidades e ter um projeto atrativo em seu portfolio.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
