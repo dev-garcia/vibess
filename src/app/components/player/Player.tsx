a39f71fa46fa7419201e947b9cb760b30f2e65c3
Cristian Garcia <garcia.ac.cristian@gmail.com>
2024-07-30 12:31:29 -0400

    feat: adiciona o componente do player e a tipagem das tracks, adiciona a página de reggaeton.

---
+++ src/app/components/player/Player.tsx
@@ -0,0 +1,171 @@
+"use client";
+
+import { Track } from "@/app/types";
+import { FC, useCallback, useEffect, useRef, useState } from "react";
+import {
+  FaPause,
+  FaPlay,
+  FaRandom,
+  FaRedo,
+  FaStepBackward,
+  FaStepForward,
+} from "react-icons/fa";
+
+interface PlayerProps {
+  trackList: Track[];
+}
+
+const Player: FC<PlayerProps> = ({ trackList }) => {
+  const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isRandom, setIsRandom] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [totalDuration, setTotalDuration] = useState(0);
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const loadTrack = useCallback(
+    (index: number) => {
+      const audio = audioRef.current;
+      if (audio) {
+        audio.src = trackList[index].url;
+        audio.load();
+        setCurrentTrackIndex(index);
+        setCurrentTime(0);
+        setIsPlaying(false);
+      }
+    },
+    [trackList],
+  );
+
+  const playTrack = useCallback(() => {
+    const audio = audioRef.current;
+    if (audio) {
+      audio.play();
+      setIsPlaying(true);
+    }
+  }, []);
+
+  const pauseTrack = useCallback(() => {
+    const audio = audioRef.current;
+    if (audio) {
+      audio.pause();
+      setIsPlaying(false);
+    }
+  }, []);
+
+  const prevTrack = useCallback(() => {
+    setCurrentTrackIndex((prevIndex) =>
+      prevIndex === 0 ? trackList.length - 1 : prevIndex - 1,
+    );
+  }, [trackList]);
+
+  const nextTrack = useCallback(() => {
+    setCurrentTrackIndex((prevIndex) =>
+      isRandom
+        ? Math.floor(Math.random() * trackList.length)
+        : (prevIndex + 1) % trackList.length,
+    );
+  }, [trackList, isRandom]);
+
+  const formatTime = (time: number) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  };
+
+  useEffect(() => {
+    const audio = audioRef.current;
+    if (audio) {
+      const handleTimeUpdate = () => setCurrentTime(audio.currentTime);
+      const handleLoadedMetadata = () => setTotalDuration(audio.duration);
+      const handleEnded = () => nextTrack();
+
+      audio.addEventListener("timeupdate", handleTimeUpdate);
+      audio.addEventListener("loadedmetadata", handleLoadedMetadata);
+      audio.addEventListener("ended", handleEnded);
+
+      return () => {
+        audio.removeEventListener("timeupdate", handleTimeUpdate);
+        audio.removeEventListener("loadedmetadata", handleLoadedMetadata);
+        audio.removeEventListener("ended", handleEnded);
+      };
+    }
+  }, [nextTrack]);
+
+  useEffect(() => {
+    loadTrack(currentTrackIndex);
+  }, [currentTrackIndex, loadTrack]);
+
+  return (
+    <div className="flex flex-col items-center justify-center p-4">
+      <div className="w-80 text-center">
+        <img
+          src={trackList[currentTrackIndex].cover}
+          alt={trackList[currentTrackIndex].title}
+          className="w-full h-48 object-cover"
+        />
+        <h2 className="mt-2 text-xl font-bold text-white">
+          {trackList[currentTrackIndex].title}
+        </h2>
+        <p className="mt-2 font-medium text-gray-400">
+          {trackList[currentTrackIndex].artist}
+        </p>
+        <audio ref={audioRef} />
+        <div className="mt-4 flex items-center justify-center space-x-4">
+          <button
+            onClick={prevTrack}
+            className="text-white hover:text-gray-400"
+          >
+            <FaStepBackward size={20} />
+          </button>
+          <button
+            onClick={isPlaying ? pauseTrack : playTrack}
+            className="mx-4 text-white hover:text-gray-400"
+          >
+            {isPlaying ? <FaPause size={20} /> : <FaPlay size={20} />}
+          </button>
+          <button
+            onClick={nextTrack}
+            className="text-white hover:text-gray-400"
+          >
+            <FaStepForward size={20} />
+          </button>
+          <button
+            onClick={() => setIsRandom(!isRandom)}
+            className="ml-4 text-white hover:text-gray-400"
+          >
+            <FaRandom size={20} />
+          </button>
+          <button
+            onClick={() => loadTrack(currentTrackIndex)}
+            className="ml-4 text-white hover:text-gray-400"
+          >
+            <FaRedo size={20} />
+          </button>
+        </div>
+        <div className="mt-4 w-full">
+          <div className="text-white">
+            <span>{formatTime(currentTime)}</span> /{" "}
+            <span>{formatTime(totalDuration)}</span>
+          </div>
+          <input
+            type="range"
+            min={0}
+            max={totalDuration}
+            value={currentTime}
+            onChange={(e) => {
+              const audio = audioRef.current;
+              if (audio) {
+                audio.currentTime = Number(e.target.value);
+                setCurrentTime(audio.currentTime);
+              }
+            }}
+            className="w-full"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Player;
--- src/app/reggaeton/page.tsx
+++ src/app/reggaeton/page.tsx
@@ -1,38 +1,66 @@
 "use client";
+
+import Player from "../components/player/Player";
+import { Track } from "../types";
+
+const trackList: Track[] = [
+  {
+    id: 1,
+    title: "Bzrp Music Sessions",
+    artist: "Bizarrap, Quevedo",
+    url: "/sons/reggaeton/01-Quevedo-BzrpMusicSessions-Vol52.mp3",
+    cover: "/images/playlists/reggaeton-cover.jpg",
+  },
+  {
+    id: 2,
+    title: "Despechá",
+    artist: "Rosalía",
+    url: "/sons/reggaeton/03-Despecha.mp3",
+    cover: "/images/playlists/reggaeton-cover.jpg",
+  },
+  {
+    id: 3,
+    title: "La Bachata",
+    artist: "Manuel Turizo",
+    cover: "/images/playlists/reggaeton-cover.jpg",
+    url: "/sons/reggaeton/04-LaBachata.mp3",
+  },
+  {
+    id: 4,
+    title: "Me Porto Bonito",
+    artist: "Bad Bunny, Chencho Corleone",
+    url: "/sons/reggaeton/05-MePortoBonito.mp3",
+    cover: "/images/playlists/reggaeton-cover.jpg",
+  },
+  {
+    id: 5,
+    title: "Monotonía",
+    artist: "Shakira, Ozuna",
+    url: "/sons/reggaeton/06-Monotonia.mp3",
+    cover: "/images/playlists/reggaeton-cover.jpg",
+  },
+];
+
 export default function Reggaeton() {
   return (
     <>
-      <div className="relative h-full w-full overflow-y-auto bg-gray-50">
+      <div className="relative h-full w-full overflow-y-auto bg-gray-50 dark:bg-gray-900">
         <div className="px-4 pt-4">
-          {/* seção 1 */}
           <section>
-            <h1>Reggaeton</h1>
+            <h1 className="text-2xl font-bold text-gray-900 dark:text-white">
+              Reggaeton
+            </h1>
           </section>
-
           <div className="my-4 grid grid-cols-1 xl:gap-4 2xl:grid-cols-2">
-            {/* seção 2 */}
             <section>
-              <h2>Seção aqui</h2>
+              <h2 className="text-xl font-semibold text-gray-900 dark:text-white">
+                Player
+              </h2>
+              <Player trackList={trackList} />
             </section>
-            {/* seção 3 */}
-            <section className="dark:text-azul-100">
-              <h2>Seção aqui</h2>
-            </section>
-          </div>
-
-          <div className="my-4 grid grid-cols-1 xl:gap-4 2xl:grid-cols-2">
-            {/* seção 4 */}
-            <section className="dark:border-gray-700 dark:bg-gray-800 dark:text-white">
-              <h2>Seção aqui</h2>
-            </section>
-            {/* seção 5 */}
-            <section className="dark:border-gray-700 dark:bg-gray-800 dark:text-white">
-              <h2>Seção aqui</h2>
-            </section>
+            {/* Outras seções */}
           </div>
         </div>
-
-        {/* <app-footer /> */}
       </div>
     </>
   );
